d401c869043f61331c64ed390c9bcc2b54c67b4d
Joel Janson <joel.janson.johansen@gmail.com>
2024-03-30 13:35:21 +0100

    updated url

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import "./App.scss";
 import VideoPlayer from "./VideoPlayer"; // Import the VideoPlayer component
 
 const videoUrls = [
-	"https://www.youtube-nocookie.com/embed/ZLT_MlV5dPw?si=2Y-G4dhqpOT8-q25&amp;start=",
-	"https://www.youtube-nocookie.com/embed/ZLT_MlV5dPw?si=2Y-G4dhqpOT8-q25&amp;start=",
-	"https://www.youtube-nocookie.com/embed/ZLT_MlV5dPw?si=2Y-G4dhqpOT8-q25&amp;start=",
-	"https://www.youtube-nocookie.com/embed/ZLT_MlV5dPw?si=2Y-G4dhqpOT8-q25&amp;start=",
+	"https://www.youtube-nocookie.com/embed/7DiEyqEdqmM?si=kLJhZlh-mYAvQbds&amp;start=",
+	"https://www.youtube-nocookie.com/embed/7DiEyqEdqmM?si=kLJhZlh-mYAvQbds&amp;start=",
+	"https://www.youtube-nocookie.com/embed/7DiEyqEdqmM?si=kLJhZlh-mYAvQbds&amp;start=",
+	"https://www.youtube-nocookie.com/embed/7DiEyqEdqmM?si=kLJhZlh-mYAvQbds&amp;start=",
 ];
 
 const App: React.FC = () => {
@@ -18,9 +18,9 @@ const App: React.FC = () => {
 		const hours = date.getHours();
 		console.log(hours);
 		const index = Math.floor(hours / 6);
-		/* const startSeconds =
-			((hours % 6) * 60 + date.getMinutes()) * 60 + date.getSeconds(); */
-		const startSeconds = 60 * (date.getMinutes() / 60);
+		const startSeconds =
+			((hours % 6) * 60 + date.getMinutes()) * 60 + date.getSeconds();
+		//const startSeconds = 60 * (date.getMinutes() / 60);
 
 		setCurrentVideoUrl(
 			`${videoUrls[index]}${startSeconds}&autoplay=1&controls=0&modestbranding=1&rel=0`
